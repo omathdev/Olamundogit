d16d6ee5ce42312e78f927891ac67fbbfc8d953b
unknown <matheusxchaves@gmail.com>
2023-04-29 16:42:40 -0300

    first commit

---
+++ SubTitulo.jsx
@@ -0,0 +1,13 @@
+import "./App.css"
+
+function SubTitulo() {
+ return (
+    <>
+    
+     <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quae ad amet veniam odio ipsam alias, fugit, iusto unde ipsa accusamus voluptatum itaque provident reprehenderit mollitia ab eum. Eos, placeat ipsam!</p>
+    
+    </>
+ )   
+}
+
+export default SubTitulo 
---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import "./App.css"
+
+const App=() => {
+  return (
+    <> 
+    <div>
+       <h1 className="Titulo">Ola Mundo</h1>
+    </div>
+    <div>
+      <h2>Ola Mundo</h2>
+    </div>
+
+    </>
+  )
+}
+
+export default App 
