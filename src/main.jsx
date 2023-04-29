9f6d9a51de86d22240eb691c9702ec7d59ed2403
omathdev <matheusxchaves@gmail.com>
2023-04-29 19:24:33 -0300

    ola amigos

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import SubTitulo from '../SubTitulo.jsx'
+import SubTitulo from './SubTitulo.jsx'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
