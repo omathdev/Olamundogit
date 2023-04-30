1ce48db75bf7c8bf61273bfdc578a8a7a09e3f91
Horaci Mota <horaciomota93@gmail.com>
2023-04-30 16:53:29 +0100

    Mensagem

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
-import "./App.css"
+import "./App.css";
 
 function App() {
   return (
     <>
-     <h1>Quem está assistindo?</h1>
-    
+      <h1>Quem está assistinssdo?</h1>
     </>
-  )
+  );
 }
 
-export default App 
+export default App;
